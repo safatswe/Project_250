89dcd2408a813d22af60321f5da2158c5709de70
Safat <farabisafat@gmail.com>
2025-03-21 01:42:34 +0600

    add signing login activity

--- app/src/main/java/com/example/quiz_app/SplashActivity.java
+++ app/src/main/java/com/example/quiz_app/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,CategoryActivity.class));
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
                 finish();
             }
         },4000);
